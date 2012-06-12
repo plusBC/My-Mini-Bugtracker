062ddfc198a8d5e077bb698f92de8105737ed4f8
Daniel Fiederling <plus@byteconsult.de>
2012-06-12 13:16:16 +0200

    DateUtils Hinzugefuegt

---
+++ MyMiniBugtracker/src/com/example/myminibugtracker/utils/DateUtils.java
@@ -0,0 +1,12 @@
+package com.example.myminibugtracker.utils;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class DateUtils {
+
+	public final static DateTimeFormatter formatterGerman = DateTimeFormat
+			.forPattern("dd.MM.YYYY HH:mm:ss");
+
+
+}
